f7c1bd7a58e08fbb1d223ff649583d92f3078a9c
Branden Bean Drake <30214543+beandrake@users.noreply.github.com>
2026-05-06 19:01:54 -0700

    Frontend can send SQL queries to the backend and get results back.  Using rollbacks to recovers from failed queries.  Still need to investigate some edge cases and do some refactoring.

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,5 +1,5 @@
 import time
-from flask import Flask
+from flask import Flask, request
 
 from db import create_connection
 from getFile import get_and_parse_data
@@ -17,39 +17,71 @@ DB_PORT_INSIDE = '5432'		# ports set in docker-compose.yml
 DB_HOST = 'db'				# the name of the service from docker-compose.yml
 
 
+class PostgresDatabase:
+	def __init__(self):
+		self.connection = create_connection(
+			DEFAULT_DATABASE,
+			DEFAULT_USER,
+			SUPERUSER_PASSWORD,
+			DB_PORT_OUTSIDE,
+			DB_HOST
+		)
+		self.cursor = self.connection.cursor();
+
+	def runQuery(self, query, values=[]):
+		"""
+		Run the query and return the results into a dictionary containing
+		two items: headers and records.
+		"""
+		self.cursor.execute(query, values)
+		headerList = [desc[0] for desc in self.cursor.description]
+		recordList = []
+		for record in self.cursor:
+			recordList.append(record)
+		
+		self.connection.commit()
+		return {
+			"headers": headerList,
+			"records": recordList
+		}
+	
+	def rollback(self):
+		self.connection.rollback()
+
+	def getCursor(self):
+		return self.cursor
+
+
 #@app.route('/api/start')
-def initialize_records():
+def initialize_records(database):
 	# create connection to database
-	connection = create_connection(
-		DEFAULT_DATABASE,
-		DEFAULT_USER,
-		SUPERUSER_PASSWORD,
-		DB_PORT_OUTSIDE,
-		DB_HOST
-	)
+	#connection = create_connection(
+	#	DEFAULT_DATABASE,
+	#	DEFAULT_USER,
+	#	SUPERUSER_PASSWORD,
+	#	DB_PORT_OUTSIDE,
+	#	DB_HOST
+	#)
 
 	# get data from web endpoint
 	rivenData = get_and_parse_data()
 
 	# connect to Postgres container
-	cursor = connection.cursor()
+	cursor = database.getCursor()
 
 	# load tables with data from the web endpoint
 	load_tables(cursor, rivenData["timestamp"], rivenData["records"])
 
 
 apiReady = False
+database = PostgresDatabase()
 print("Initializing records...")
-initialize_records()
+initialize_records(database)
 print("Records initialized.")
 apiReady = True
 
 
 app = Flask(__name__)
-
-@app.route('/')
-def hello():
-	return "Hello World!"
 
 @app.route('/api/time')
 def get_current_time():
@@ -58,3 +90,32 @@ def get_current_time():
 @app.route('/api/ready')
 def get_api_ready():
 	return {'ready': apiReady}
+
+
+# Runs any query the user submits.
+@app.route('/api/query', methods=['GET', 'POST'] )
+def get_api_query():
+	dictionary = request.args
+	print(dictionary)
+	query = dictionary['query']
+	print(query)
+	
+	#database = PostgresDatabase()
+	try:
+		results = database.runQuery(query)
+		print(results)
+	except Exception as e:
+		print(f'Error: {e}')
+		database.rollback()	
+
+	return results
+
+
+
+
+
+
+
+
+
+
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,12 @@ function App() {
 	const [count, setCount] = useState(0);
 	const [currentTime, setCurrentTime] = useState(0);
 	//const backendURL = import.meta.env.VITE_BACKEND_URL; // https://vite.dev/guide/env-and-mode.html
+
+	const defaultQuery = `
+	SELECT *
+	FROM information_schema.tables
+	WHERE table_schema= 'public'
+	ORDER BY table_name;`;
 	
 
 	// NOTE: React knows where to send fetch requests, see vite.config.js	
@@ -25,10 +31,9 @@ function App() {
 	}
 
 	function checkReadyAPI() {
-		console.log("Sending query...");
+		console.log("Sending request...");
 		fetch
 		(
-			//backendURL + 
 			'/api/ready'
 		).then(
 			response => {
@@ -51,23 +56,48 @@ function App() {
 	}
 
 
+	function runQuery(query) {
+		console.log("Sending query...");
+		fetch(
+			'/api/query?' + new URLSearchParams( {query: query,} )
+		).then(
+			response => {
+				if (response.ok) {
+					return response.json();
+				}
+				throw response; // will be handled by catch
+			}
+		).then(
+			data => {
+				console.log("Query results received!");
+				setItemGrid(data);
+				console.log(data);
+			}
+		).catch(
+			exception => {
+				console.log("Couldn't connect!");
+				throw exception;
+			}
+		);
+	}
+
+
 
 	return (
 		<>
-			<p>if you see this then congrats, it's working too good</p>
 			<div className="upper">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
 				<button onClick={checkReadyAPI}>
-					Sample Query
+					Test Connection
 				</button>
 				<p>
 					Edit <code>src/App.jsx</code> and save to test HMR.
 				</p>
 				<QueryForm
-					runQuery={performSampleQuery}
-					defaultQueryText={"put a query here please"}
+					runQuery={runQuery}
+					defaultQueryText={defaultQuery}
 				/>			
 			</div>
 			<div className="lower">
